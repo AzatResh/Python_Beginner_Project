209c9592a107fd72af6eb38ac501e8c48045e245
AzatResh <Azat.R.2003@mail.ru>
2024-10-20 16:31:42 +0600

    add POST, PUT, PATC and DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,15 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from profiles_api import serializers
+
 class HelloApiView(APIView):
-    """Test API VIEW"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
-        """Returns a list of apiView features"""
+
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete',
             'Is similar to a traditional Django View',
@@ -16,3 +20,26 @@ class HelloApiView(APIView):
             'message': 'Hello!',
             'an_apiview': an_apiview
         })
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello, {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
