0f541a3af3f9c0d19ca56cd6755d65f12f33767b
AzatResh <Azat.R.2003@mail.ru>
2024-10-23 17:57:44 +0600

    add search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
 
@@ -88,3 +93,11 @@ class HelloViewSet(viewsets.ViewSet):
 
     def destroy(self, request, pk=None):
         return Response({'http-method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
